c818d68746f8dbdc74f73e52a59ae79ec2542b88
ahaNoDobre <gubany1@stud.uniza.sk>
2025-12-20 21:29:42 +0100

    Add UnassignManagerFromNodeTests use-case with tests.

---
+++ CompanyManagement.Application/DTOs/UnassignManagerFromNodeRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace CompanyManagement.Application.DTOs
+{
+    public class UnassignManagerFromNodeRequest
+    {
+        public Guid NodeId { get; init; }
+    }
+}
---
+++ CompanyManagement.Application/UseCases/UnassignManagerFromNode.cs
@@ -0,0 +1,44 @@
+﻿using CompanyManagement.Application.Abstractions.Repositories;
+using CompanyManagement.Application.DTOs;
+
+namespace CompanyManagement.Application.UseCases
+{
+    public class UnassignManagerFromNode
+    {
+        private readonly INodeRepository _nodeRepository;
+
+        public UnassignManagerFromNode(INodeRepository nodeRepository)
+        {
+            _nodeRepository = nodeRepository;
+        }
+
+        /// <summary>
+        /// Removes the assigned manager (leader) from an organizational node.
+        /// </summary>
+        /// <param name="request">
+        /// Request containing the identifier of the node from which the manager should be unassigned.
+        /// </param>
+        /// <exception cref="ArgumentException">
+        /// Thrown when the specified node does not exist.
+        /// </exception>
+        /// <remarks>
+        /// If the node does not currently have a manager assigned,
+        /// the operation completes without making any changes.
+        /// </remarks>
+        public async Task ExecuteAsync(UnassignManagerFromNodeRequest request)
+        {
+            var node = await _nodeRepository.GetByIdAsync(request.NodeId)
+                ?? throw new ArgumentException("Node not found");
+
+
+            if (node.LeaderEmployeeId == null)
+            {
+                return;
+            }
+
+            node.AssignLeader(null);
+
+            await _nodeRepository.UpdateAsync(node);
+        }
+    }
+}
